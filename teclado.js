27146a5af2d6cc151eadcd0f33edfaf764574f60
BPGTT <grupobpgtt@gmail.com>
2023-05-19 20:57:13 -0300

    teclado

---
+++ teclado.js
@@ -0,0 +1,14 @@
+// function aperta(lt){
+//     document.getElementById("aperta").innerHTML = "Você apertou a letra "+lt;
+// }
+
+
+function aperta(lt){
+       document.getElementById("aperta").innerHTML = `Você apertou a letra ${lt}`;
+
+}
+
+
+function soltar(lt){
+    document.getElementById("solta").innerHTML = "Você soltou a tecla "+lt
+}
